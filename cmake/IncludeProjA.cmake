f64c1883fb4a69bec6669220d054c826b1004fc4
William Sciaroni <wsciaroni@protonmail.com>
2023-07-04 22:29:35 +0000

    Explicitly grab version 0.0.0 of proj A

--- cmake/IncludeProjA.cmake
+++ cmake/IncludeProjA.cmake
@@ -4,7 +4,7 @@ macro(include_proj_a)
     FetchContent_Declare(
         ProjA
         GIT_REPOSITORY https://github.com/wsciaroni/projA.git
-        GIT_TAG master
+        GIT_TAG 0.0.0
     )
 
     # Check to see if googletest has already been populated so we only populate it once
